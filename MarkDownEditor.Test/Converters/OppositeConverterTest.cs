2aa5ccb35dd5e33e2b45900204d37abba5c9ec70
David Prudek <prudek.david@gmail.com>
2020-02-24 16:50:52 +0300

    Added new xUnit test project

---
+++ MarkDownEditor.Test/Converters/OppositeConverterTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarkDownEditor.Test.Converters
+{
+    public class OppositeConverterTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
