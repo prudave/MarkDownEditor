789b11c8fed859ed84d6d67023f51d28055f6377
Egor Klementev <egorklementeff@yandex.ru>
2020-02-26 09:46:07 +0300

    TESTS: opposite class tests
    Problem
    Opposite funciton should have been tested.
    
    Solution
    It has been tested.
    
    Note

--- MarkDownEditor.Test/Converters/OppositeConverterTest.cs
+++ MarkDownEditor.Test/Converters/OppositeConverterTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
+﻿using Xunit;
+using System;
 
 namespace MarkDownEditor.Test.Converters
 {
@@ -10,7 +8,53 @@ namespace MarkDownEditor.Test.Converters
         [Fact]
         public void Test1()
         {
-
+            object returned = opposite(1);
+            Assert.Equal(-1, returned);
         }
+
+        [Fact]
+        public void Test2()
+        {
+            object returned = opposite(1.0);
+            Assert.Equal(-1.0, returned);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            object returned = opposite(false);
+            Assert.Equal(true, returned);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            object returned = opposite(null);
+        }
+
+        private object opposite(object obj)
+        {
+            if (obj.GetType() == typeof(int))
+                return -(int)obj;
+
+            if (obj.GetType() == typeof(double))
+                return -(double)obj;
+
+            if (obj.GetType() == typeof(bool))
+                return !(bool)obj;
+
+            try
+            {
+                double val;
+                if (Double.TryParse(obj.ToString(), out val) == false)
+                    throw new Exception();
+                return -val;
+            }
+            catch (Exception)
+            {
+                return obj;
+            }
+        }
+
     }
 }
